4cd8c98764e15562715856331a4818a0cb99ceb2
Mateusz 135029 <chx95907@student.chorzow.merito.pl>
2024-03-10 13:13:41 +0100

    Dodaj pliki projektów.

---
+++ Singleton/Logger.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjectPatterns
+{
+    public sealed class SingletonLogger
+    {
+        private static SingletonLogger _instance;
+        private List<string> logMessages;
+
+        private SingletonLogger()
+        {
+            logMessages = new List<string>();
+        }
+
+        public static SingletonLogger GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new SingletonLogger();
+            }
+
+            return _instance;
+        }
+
+        public void LogMessage(string message)
+        {
+            logMessages.Add(message);
+        }
+
+        public void PrintLogMessages()
+        {
+            Console.WriteLine("Logged messages:");
+            foreach (var message in logMessages)
+            {
+                Console.WriteLine(message);
+            }
+        }
+    }
+
+    class Logger
+    {
+        static void Main(string[] args)
+        {
+            SingletonLogger logger1 = SingletonLogger.GetInstance();
+            SingletonLogger logger2 = SingletonLogger.GetInstance();
+
+            logger1.LogMessage("Test użycia 1. logera");
+            logger2.LogMessage("Test użycia 2. logera");
+            logger1.LogMessage("Message using 1st logger");
+            logger2.LogMessage("Message using 2nd logger");
+
+            if (logger1 == logger2)
+            {
+                Console.WriteLine("Singleton Logger works");
+            }
+            else
+            {
+                Console.WriteLine("Singleton Logger failed");
+            }
+
+            logger1.PrintLogMessages();
+        }
+    }
+}
---
+++ Singleton/Printer.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Singleton
+{
+    public class PrintJob
+    {
+        public string Content { get; }
+
+        public PrintJob(string content)
+        {
+            Content = content;
+        }
+    }
+
+    public sealed class PrintBuffer
+    {
+        private static PrintBuffer _instance;
+        private Queue<PrintJob> printQueue;
+
+        private PrintBuffer()
+        {
+            printQueue = new Queue<PrintJob>();
+        }
+
+        public static PrintBuffer GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new PrintBuffer();
+            }
+
+            return _instance;
+        }
+
+        public void AddPrintJob(PrintJob job)
+        {
+            printQueue.Enqueue(job);
+        }
+
+        public void ExecutePrintJobs()
+        {
+            while (printQueue.Count > 0)
+            {
+                var job = printQueue.Dequeue();
+                Console.WriteLine("Printing: " + job.Content);
+            }
+        }
+    }
+
+    class Printer
+    {
+        static void Main(string[] args)
+        {
+            PrintBuffer printBuffer1 = PrintBuffer.GetInstance();
+            PrintBuffer printBuffer2 = PrintBuffer.GetInstance();
+
+            if (printBuffer1 == printBuffer2)
+            {
+                Console.WriteLine("Singleton Print Buffer works");
+            }
+            else
+            {
+                Console.WriteLine("Singleton Print Buffer failed");
+            }
+
+            printBuffer1.AddPrintJob(new PrintJob("Document 1"));
+            printBuffer1.AddPrintJob(new PrintJob("Document 2"));
+
+            printBuffer1.ExecutePrintJobs();
+        }
+    }
+}
